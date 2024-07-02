ea245bf8e2b6b40637412096e9ff39a7d9928828
omkar nath dubey <omkardubey1530@gmail.com>
2024-07-02 14:49:03 +0530

    commit it

--- src/EverWrite/config.js
+++ src/EverWrite/config.js
@@ -16,27 +16,107 @@ export class Service {
 
   async createPost({ title, slug, content, featureImage, status, userId }) {
     try {
-      return (
-        await this.databases.createDocument(
-          conf.everWriteDatabaseId, 
-          conf.everWriteCollectionId,
-          slug,
-          {
-            title,
-            content,
-            featureImage,
-            status,
-            userId,
-          }
-        )
+      return await this.databases.createDocument(
+        conf.everWriteDatabaseId,
+        conf.everWriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featureImage,
+          status,
+          userId,
+        }
       );
     } catch (error) {
       console.log("AppWrite service :: createPost :: error", error);
     }
   }
 
-  async updatePost(){
-   
+  async updatePost(slug, { title, content, featureImage, status }) {
+    try {
+      return await this.databases.updateDocument(
+        conf.everWriteDatabaseId,
+        conf.everWriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featureImage,
+          status,
+        }
+      );
+    } catch (error) {
+      console.log("AppWrite service :: createPost :: error", error);
+    }
+  }
+
+  async deletePost(slug) {
+    try {
+      await this.databases.deleteDocument(
+        conf.everWriteDatabaseId,
+        conf.everWriteCollectionId,
+        slug
+      );
+      return true;
+    } catch (error) {
+      console.log("AppWrite service :: createPost :: error", error);
+      return false;
+    }
+  }
+
+  async getPost(slug) {
+    try {
+      return await this.databases.getDocument(
+        conf.everWriteDatabaseId,
+        conf.everWriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.log("AppWrite service :: createPost :: error", error);
+      return false;
+    }
+  }
+
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listDocuments(
+        conf.everWriteDatabaseId,
+        conf.everWriteCollectionId,
+        queries
+      );
+    } catch (error) {
+      console.log("AppWrite service :: createPost :: error", error);
+      return false;
+    }
+  }
+
+  // file upload methods
+  async uploadFile(file) {
+    try {
+      return await this.bucket.createFile(
+        conf.everWriteBucketId,
+        ID.unique(),
+        file
+      );
+    } catch (error) {
+      console.log("AppWrite service :: createPost :: error", error);
+      return false;
+    }
+  }
+
+  async deleteFile(fileId) {
+    try {
+      await this.bucket.deleteFile(conf.everWriteBucketId, fileId);
+      return true;
+    } catch (error) {
+      console.log("AppWrite service :: createPost :: error", error);
+      return false;
+    }
+  }
+
+  getFilePreview(fileId) {
+    return this.bucket.getFilePreview(conf.everWriteBucketId, fileId);
   }
 }
 
