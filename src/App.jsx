9493416cfa45388fb3d648cfc6ee265e3b800c86
omkar nath dubey <omkardubey1530@gmail.com>
2024-07-06 00:05:02 +0530

    commit all  file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
-
-import './App.css'
+import React from "react";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./EverWrite/auth";
+import { login, logout } from "./store/authSlice";
+import "./App.css";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  console.log(import.meta.env.VITE_EVERWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-    <h1>This is a Ever Write App</h1>
-    </>
-  )
+  useEffect(() => {
+    authService
+      .GetCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block ">
+        <Header />
+        <main>
+         TODO: <Outlet /> 
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
