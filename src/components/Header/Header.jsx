199a25e0032487d7afc84e460922d758a15a86a3
omkar nath dubey <omkardubey1530@gmail.com>
2024-07-08 14:09:18 +0530

    commit changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -35,7 +35,38 @@ const Header = () => {
       active: authStatus,
     },
   ];
-  return <div>Header</div>;
+  return (
+    <header className="py-3 shadow bg-gray-500">
+      <Container>
+        <nav className="flex">
+          <div className="mr-4">
+            <Link to="/">
+              <Logo width="70px" />
+            </Link>
+          </div>
+          <ul className="flex ml-auto">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    className="inline-block py-2 px-6 duration-200 hover:bg-blue-100 rounded-full "
+                    onClick={() => navigate(item.slug)}
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn /> 
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
