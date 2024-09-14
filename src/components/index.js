18e78acfc4522eadcf5a02a0aaa4a2a76a98ddc3
OmkarDubey07 <omkardubey1530@gmail.com>
2024-09-14 17:26:56 +0530

    commit all

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,9 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
+import Login from "./Login";
+import PostCard from './PostCard'; 
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export { Header, Footer, Container, Logo, LogoutBtn, Button, Input, Login , PostCard};
